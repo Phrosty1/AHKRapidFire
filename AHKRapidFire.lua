25272213ea7fa03778abfbfe9caa9a1905572ab8
Phrosty1 <istas.phrost@gmail.com>
2021-02-03 11:49:28 -0500

    new logging

--- AHKRapidFire.lua
+++ AHKRapidFire.lua
@@ -9,7 +9,30 @@ local function dmsg(txt)
 	d((GetGameTimeMilliseconds() - ms_time) .. ") " .. txt)
 	ms_time = GetGameTimeMilliseconds()
 end
+local function dump(o)
+	if type(o) == 'table' then
+		local s = '{ '
+		for k,v in pairs(o) do
+			if type(k) ~= 'number' then k = '"'..k..'"' end
+			s = s .. '['..k..'] = ' .. dump(v) .. ','
+		end
+		return s .. '} '
+	else
+		return tostring(o)
+	end
+end
+local function LogValues(eventName, args)
+	local sep = ";"
+	local ms_log_time = GetGameTimeMilliseconds()
+	local strArgs = tostring(ms_log_time)..sep..eventName
+	for _,value in pairs(args) do strArgs = strArgs..sep..tostring(value) end
+	AHKRapidFire.savedVars.log[AHKRapidFire.savedVars.logIdx] = strArgs
+	AHKRapidFire.savedVars.logIdx = AHKRapidFire.savedVars.logIdx + 1
+end
 local keepfiring = false
+local function LClick() dmsg("Mouse Click") ptk.SetIndOnFor(ptk.VM_BTN_LEFT, 20) end
+local function Press1() dmsg("Press 1") ptk.SetIndOnFor(ptk.VK_1, 20) end
+local function BarSwap() dmsg("Press backquote") ptk.SetIndOnFor(ptk.VK_BACK_QUOTE, 20) end
 
 
 -- EVENT_COMBAT_EVENT (number eventCode, number ActionResult result, boolean isError, string abilityName, number abilityGraphic, number ActionSlotType abilityActionSlotType, string sourceName, number CombatUnitType sourceType, string targetName, number CombatUnitType targetType, number hitValue, number CombatMechanicType powerType, number DamageType damageType, boolean log, number sourceUnitId, number targetUnitId, number abilityId, number overflow)
@@ -141,7 +164,90 @@ function AHKRapidFire.GetSlotCSVLock()
 	end
 	return str
 end
+
+local msLightUsed = 0
+local isLightReady = true
 function AHKRapidFire:OpenFire()
+	keepfiring = true
+	if false then
+		BarSwap()
+		EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_WEAPON_PAIR_LOCK_CHANGED, function(_, isLocked)
+				if not isLocked then -- At least means that bar can be flipped again
+					BarSwap()
+				end
+			end)
+	end
+
+	if false then
+		LClick()
+		EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_ACTION_SLOT_ABILITY_USED, function(_, slotIdx)
+				if slotIdx == 1 then
+					isLightReady = true
+					msLightUsed = GetGameTimeMilliseconds()
+					Press1()
+				end
+			end)
+		EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_ACTION_UPDATE_COOLDOWNS, function()
+				if isLightReady and GetGameTimeMilliseconds() >= msLightUsed + 600 then
+					isLightReady = false
+					LClick()
+					d({HasRequirementFailure(1),HasTargetFailure(1),HasWeaponSlotFailure(1),IsSlotLocked(1)})
+
+				end
+			end)
+
+	end
+
+	if false then
+		EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_ACTION_SLOT_ABILITY_USED, function(_, slotIdx)
+				dmsg("EVENT_ACTION_SLOT_ABILITY_USED "..tostring(slotIdx))
+			end)
+		--500 too short
+		--600 too short
+		--650 a little too short
+		--680 a little too short
+		--690 skips every 10th ish
+		--700 works. 7600dps
+		--1000 works. 5000dps
+		EVENT_MANAGER:RegisterForUpdate("presslots", 700, LClick)
+		LClick()
+	end
+
+	if true then
+		EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_ACTION_SLOT_ABILITY_USED, function(_, slotIdx)
+				dmsg("EVENT_ACTION_SLOT_ABILITY_USED "..tostring(slotIdx))
+			end)
+		EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_ACTION_UPDATE_COOLDOWNS, function()
+				dmsg("EVENT_ACTION_UPDATE_COOLDOWNS "..AHKRapidFire.GetSlotCSVInfo())
+			end)
+
+		--1000 works
+		EVENT_MANAGER:RegisterForUpdate("presslots", 900, function()
+				zo_callLater(LClick, 0)
+				zo_callLater(Press1, 50)
+			end)
+		zo_callLater(LClick, 0)
+		zo_callLater(Press1, 50)
+	end
+
+
+	--zo_callLater(function()
+	--		BarSwap()
+	--	end, 500)
+	--EVENT_MANAGER:RegisterForUpdate("presslots", 1000, function()
+	--		LClick()
+	--	end)
+
+end
+function AHKRapidFire:CeaseFire()
+	keepfiring = false
+	EVENT_MANAGER:UnregisterForEvent(AHKRapidFire.name, EVENT_WEAPON_PAIR_LOCK_CHANGED)
+	EVENT_MANAGER:UnregisterForEvent(AHKRapidFire.name, EVENT_ACTION_SLOT_ABILITY_USED)
+	EVENT_MANAGER:UnregisterForEvent(AHKRapidFire.name, EVENT_ACTION_UPDATE_COOLDOWNS)
+	EVENT_MANAGER:UnregisterForUpdate("presslots")
+	d("CeaseFire ------------------------------")
+end
+function AHKRapidFire:OpenFireOld()
 	AHKRapidFire.PopulateActions()
 	keepfiring = true
 	dmsg("OpenFire ------------------------------")
@@ -159,7 +265,7 @@ function AHKRapidFire:OpenFire()
 	dmsg("Mouse Click") d(AHKRapidFire.GetSlotCSVLock()) ptk.SetIndOnFor(ptk.VM_BTN_LEFT, 30)
 	zo_callLater(function() dmsg("Press 1") d(AHKRapidFire.GetSlotCSVLock()) ptk.SetIndOnFor(ptk.VK_1, 30) end, 400)
 end
-function AHKRapidFire:CeaseFire()
+function AHKRapidFire:CeaseFireOld()
 	keepfiring = false
 	d("CeaseFire ------------------------------")
 	EVENT_MANAGER:UnregisterForEvent(AHKRapidFire.name, EVENT_ACTION_SLOT_ABILITY_USED)
@@ -193,8 +299,12 @@ function AHKRapidFire.OnSlotAbilityUsed(eventCode, actionSlotIndex) -- EVENT_ACT
 		local usedAction = AHKRapidFire.availableActions[slotName]
 		if usedAction ~= nil then
 			local msNow = GetGameTimeMilliseconds()
+			--dmsg(dump(AHKRapidFire.availableActions))
 			for action in pairs(AHKRapidFire.availableActions) do
-				if action.idx == 1 then
+				if action.idx == nil then
+					d(dump(action))
+					--action.holdUntil = math.max(action.holdUntil, msNow + usedAction.delayLightAttackFor)
+				elseif action.idx == 1 then
 					action.holdUntil = math.max(action.holdUntil, msNow + usedAction.delayLightAttackFor)
 				elseif action.idx >= 3 and action.idx <= 7 then
 					action.holdUntil = math.max(action.holdUntil, msNow + usedAction.delayBarActionFor)
@@ -244,6 +354,11 @@ function AHKRapidFire:Initialize()
 	--SLASH_COMMANDS["/pd"] = AHKRapidFire.PTK.PixelDemo
 	--d("FindmeInitText") -- would not show. wait until player active
 	ZO_CreateStringId("SI_BINDING_NAME_RF_FIRING", "Rapid Firing")
+	if AHKRapidFire.savedVars.log == nil or AHKRapidFire.savedVars.logIdx == nil or true then
+		AHKRapidFire.savedVars.log = {}
+		AHKRapidFire.savedVars.logIdx = 1
+	end
+
 
 	--EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_GAME_CAMERA_UI_MODE_CHANGED, OnEventUiModeChanged)
 	--EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_PLAYER_COMBAT_STATE, OnEventCombatStateChanged)
@@ -256,6 +371,17 @@ function AHKRapidFire:Initialize()
 	--EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_PLAYER_STUNNED_STATE_CHANGED, OnEventStunStateChanged)
 	--EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_COMBAT_EVENT, OnEventCombatEvent)
 	--EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name, EVENT_RETICLE_TARGET_CHANGED, OnEventReticleChanged)
+
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_ACTION_SLOT_ABILITY_USED, function(...) LogValues("EVENT_ACTION_SLOT_ABILITY_USED", {...}) end)
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_ACTION_UPDATE_COOLDOWNS, function(...) LogValues("EVENT_ACTION_UPDATE_COOLDOWNS", {...}) end)
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_ACTION_SLOT_STATE_UPDATED, function(...) LogValues("EVENT_ACTION_SLOT_STATE_UPDATED", {...}) end)
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_EFFECT_CHANGED, function(...) LogValues("EVENT_EFFECT_CHANGED", {...}) end)
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_WEAPON_PAIR_LOCK_CHANGED, function(...) LogValues("EVENT_WEAPON_PAIR_LOCK_CHANGED", {...}) end)
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_ACTION_SLOT_UPDATED, function(...) LogValues("EVENT_ACTION_SLOT_UPDATED", {...}) end)
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_COMBAT_EVENT, function(...) LogValues("EVENT_COMBAT_EVENT", {...}) end)
+
+	EVENT_MANAGER:RegisterForEvent(AHKRapidFire.name.."LOG", EVENT_HOTBAR_SLOT_CHANGE_REQUESTED, function(...) LogValues("EVENT_HOTBAR_SLOT_CHANGE_REQUESTED", {...}) end)
+
 
 end
 
